40a51048ba9cc19ed3c5560260d830be16301c48
KocurMaros <kocur.maros@gmail.com>
2022-10-12 22:38:52 +0200

    clear

--- src/meno_matky_za_slobodna/src/turtle_control.cpp
+++ src/meno_matky_za_slobodna/src/turtle_control.cpp
@@ -1,4 +1,5 @@
 #include "meno_matky_za_slobodna/turtle_control.h"
+#include <std_srvs/Empty.h>
 
 TurtleControl::TurtleControl(){
 
@@ -102,8 +103,8 @@ bool TurtleControl::drawCallback(meno_matky_za_slobodna::Draw::Request &req, men
 void TurtleControl::poseCallback(const turtlesim::Pose::ConstPtr& msg)
 {    
     ros::NodeHandle n;
-    ros::ServiceClient clearClient = n.serviceClient<meno_matky_za_slobodna::Empty>("/clear");
-    meno_matky_za_slobodna::Empty srv;
+    ros::ServiceClient clearClient = n.serviceClient<std_srvs::Empty>("/clear");
+    std_srvs::Empty srv;
     if ((msg->x >= WINDOW_EDGE)||(msg->y >= WINDOW_EDGE)||(msg->x <= 0)||(msg->y <= 0))
     {
         velocity_msg_.linear.x = 0;

5cba9641a959ff4e413d7cb9e7baac554aa69eef
KocurMaros <kocur.maros@gmail.com>
2022-10-27 13:33:22 +0200

    co edo kazal

--- src/rrm_cv3_kocur/src/forward_kinematics.cpp
+++ src/rrm_cv3_kocur/src/forward_kinematics.cpp
@@ -44,7 +44,7 @@ void ForwardKinematics::broadcastTf(){
     // Calculated forward kinematic tool0 -> base_link
     transform.setOrigin( position_ );
     transform.setRotation(orientation_);
-    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "tool0"));
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_1", "tool0"));
 
     // Links
     transform.setOrigin( tf::Vector3(0, 0, 0.1015));

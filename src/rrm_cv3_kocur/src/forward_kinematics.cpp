a457cb2d297fe0c0461724b6696434ab5a9f2ea3
KocurMaros <kocur.maros@gmail.com>
2022-10-29 21:27:17 +0200

    rotacie

--- src/rrm_cv3_kocur/src/forward_kinematics.cpp
+++ src/rrm_cv3_kocur/src/forward_kinematics.cpp
@@ -73,7 +73,7 @@ void ForwardKinematics::jointCallback(const sensor_msgs::JointState::ConstPtr& m
 {
     joint_state_ = *msg;
 
-    Eigen::MatrixXd T0 = (createRz(joint_state_.position[0])) * (createRz(joint_state_.position[1]))* (createRz(joint_state_.position[2]));// * createTz(joint_state_.position[3]) ;//+ createRy(joint_state_.position[3] * createTz(L3));
+    Eigen::MatrixXd T0 = (createRz(joint_state_.position[0])) * (createRy(joint_state_.position[1]))* (createRy(joint_state_.position[2]));// * createTz(joint_state_.position[3]) ;//+ createRy(joint_state_.position[3] * createTz(L3));
 
     // convert rotation matrix to tf matrix
     tf::Matrix3x3 tf3d;

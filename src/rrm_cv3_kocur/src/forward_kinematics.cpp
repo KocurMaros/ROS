569c14a168a3cf41f41bccc10a527f79ba81c5a2
KocurMaros <kocur.maros@gmail.com>
2022-11-02 22:00:06 +0100

    barzo error

--- src/rrm_cv3_kocur/src/forward_kinematics.cpp
+++ src/rrm_cv3_kocur/src/forward_kinematics.cpp
@@ -103,14 +103,14 @@ void ForwardKinematics::jointCallback(const sensor_msgs::JointState::ConstPtr& m
     tf::Matrix3x3 tf3d;
     tf::Matrix3x3 tf3d_to_joint2;
     tf::Matrix3x3 tf3d_to_joint3;
-    tf::Matri3x3 tf3d_dh
+    tf::Matrix3x3 tf3d_dh;
     // tf::Matrix3x3 tf3d_to_joint4;
 
     Eigen::MatrixXd dh;
     tf3d_dh.setValue(static_cast<double>(dh(0,0)), static_cast<double>(dh(0,1)), static_cast<double>(dh(0,2)),
                 static_cast<double>(dh(1,0)), static_cast<double>(dh(1,1)), static_cast<double>(dh(1,2)),
                 static_cast<double>(dh(2,0)), static_cast<double>(dh(2,1)), static_cast<double>(dh(2,2)));
-    dh = DH(0,0,0,join_state_.position[0])*DH(,0,90*M_PI/180,L1,joint_state_.position[1])*DH(L2+joint_state_.position[2],0,L2+joint_state_.position[2],0)*DH(0,0,L3,M_PI+joint_state_.position[3]);
+    dh = DH(0,0,0,joint_state_.position[0])*DH(0,90*M_PI/180,L1,joint_state_.position[1])*DH(L2+joint_state_.position[2],0,L2+joint_state_.position[2],0)*DH(0,0,L3,M_PI+joint_state_.position[3]);
     
     tf3d.setValue(static_cast<double>(T0(0,0)), static_cast<double>(T0(0,1)), static_cast<double>(T0(0,2)),
                   static_cast<double>(T0(1,0)), static_cast<double>(T0(1,1)), static_cast<double>(T0(1,2)),

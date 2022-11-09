aa73ac0277101cd71095371eb9abb19686b5a129
KocurMaros <kocur.maros@gmail.com>
2022-11-09 22:54:04 +0100

    pato

---
+++ src/rrm_cv3_ridilla/src/forward_kinematics.cpp
@@ -0,0 +1,157 @@
+#include "rrm_cv3_ridilla/forward_kinematics.h"
+
+using namespace matrix;
+
+ForwardKinematics::ForwardKinematics() : position_(0, 0, L1 + L2 + L3 + L4){
+
+    // ROS node handler
+    ros::NodeHandle n;
+
+    // Creating subscriber, publisher and service server
+    joint_sub_ = n.subscribe("joint_states", 5, &ForwardKinematics::jointCallback, this);
+
+    // Resize joint_state array and initialize with value 0
+    joint_state_.position.resize(3, 0);
+
+    orientation_.setRPY(0, 0, 0);
+    position_ = tf::Vector3(0, 0, 0.0);
+
+    orientation2_.setRPY(0, 0, 0);
+    position2_ = tf::Vector3(0, 0, 0.0);
+
+    orientation3_.setRPY(0, 0, 0);
+    position3_ = tf::Vector3(0, 0, 0.0);
+}
+
+void ForwardKinematics::broadcastTf(){
+
+    //Create transformation, set origin and rotation and finally send
+    tf::Transform transform;
+
+    transform.setOrigin( tf::Vector3(0, 0, 0.0));
+    tf::Quaternion q;
+    q.setRPY(0,0,joint_state_.position[0]);
+    transform.setRotation(q);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "joint_1"));
+
+    // transform.setOrigin( tf::Vector3(0, 0, 0.203));
+    // q.setRPY(0,joint_state_.position[1],0);
+    // transform.setRotation(q);
+    // broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_1", "joint_2"));
+
+    // transform.setOrigin( tf::Vector3(0, 0, 0.178 + joint_state_.position[2]));
+    // q.setRPY(0,0,0);
+    // transform.setRotation(q);
+    // broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_2", "joint_3"));
+
+    transform.setOrigin( tf::Vector3(0, 0, 0.178));
+    q.setRPY(0,joint_state_.position[3],0);
+    transform.setRotation(q);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_3", "joint_4"));
+
+    // transform.setOrigin( tf::Vector3(0, 0, 0.178));
+    // q.setRPY(0,0,0);
+    // transform.setRotation(q);
+    // broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_4", "tool0"));
+     // Calculated forward kinematic tool0 -> base_link
+    transform.setOrigin( position_ );
+    transform.setRotation(orientation_);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "tool0"));
+
+    transform.setOrigin( position2_ );
+    transform.setRotation(orientation2_);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "joint_2"));
+
+    transform.setOrigin( position3_ );
+    transform.setRotation(orientation3_);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "joint_3"));
+
+    // Links
+    transform.setOrigin( tf::Vector3(0, 0, 0.1015));
+    q.setRPY(0,0,0);
+    transform.setRotation(q);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_1", "link1"));
+
+    transform.setOrigin( tf::Vector3(0, 0, 0.089));
+    q.setRPY(0,0,0);
+    transform.setRotation(q);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_2", "link2"));
+
+    transform.setOrigin( tf::Vector3(0, 0, 0.089));
+    q.setRPY(0,0,0);
+    transform.setRotation(q);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_3", "link3"));
+
+    transform.setOrigin( tf::Vector3(0, 0, 0.089));
+    q.setRPY(0,0,0);
+    transform.setRotation(q);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_4", "link4"));
+
+}
+
+
+void ForwardKinematics::jointCallback(const sensor_msgs::JointState::ConstPtr& msg)
+{
+    joint_state_ = *msg;
+
+    Eigen::MatrixXd T0 = createRz(joint_state_.position[0]) * createTz(L1) * createRy(joint_state_.position[1]) * createTz(L2) * createTz(joint_state_.position[2]) * createTz(L3) * createRy(joint_state_.position[3]);
+    Eigen::MatrixXd j2 = createRz(joint_state_.position[0]) * createTz(L1) * createRy(joint_state_.position[1]);
+    Eigen::MatrixXd j3 = createRz(joint_state_.position[0]) * createTz(L1) * createRy(joint_state_.position[1]) * createTz(L2) * createTz(L3+joint_state_.position[2]);
+
+    // convert rotation matrix to tf matrix
+    tf::Matrix3x3 tf3d;
+    tf::Matrix3x3 tf3d_j2;
+    tf::Matrix3x3 tf3d_j3;
+    tf3d.setValue(static_cast<double>(T0(0,0)), static_cast<double>(T0(0,1)), static_cast<double>(T0(0,2)),
+                  static_cast<double>(T0(1,0)), static_cast<double>(T0(1,1)), static_cast<double>(T0(1,2)),
+                  static_cast<double>(T0(2,0)), static_cast<double>(T0(2,1)), static_cast<double>(T0(2,2)));
+
+    // Convert to quternion
+    tf3d.getRotation(orientation_);
+
+    tf3d_j2.setValue(static_cast<double>(j2(0,0)), static_cast<double>(j2(0,1)), static_cast<double>(j2(0,2)),
+                  static_cast<double>(j2(1,0)), static_cast<double>(j2(1,1)), static_cast<double>(j2(1,2)),
+                  static_cast<double>(j2(2,0)), static_cast<double>(j2(2,1)), static_cast<double>(j2(2,2)));
+
+    // Convert to quternion
+    tf3d_j2.getRotation(orientation2_);
+
+    tf3d_j3.setValue(static_cast<double>(j3(0,0)), static_cast<double>(j3(0,1)), static_cast<double>(j3(0,2)),
+                  static_cast<double>(j3(1,0)), static_cast<double>(j3(1,1)), static_cast<double>(j3(1,2)),
+                  static_cast<double>(j3(2,0)), static_cast<double>(j3(2,1)), static_cast<double>(j3(2,2)));
+
+    // Convert to quternion
+    tf3d_j3.getRotation(orientation3_);
+
+
+
+    // Calculate position
+    Eigen::MatrixXd p(4,1);
+    p(0,0) = 0;
+    p(1,0) = 0;
+    p(2,0) = L4;
+    p(3,0) = 1;
+
+    Eigen::MatrixXd p2(4,1);
+    p2(0,0) = 0;
+    p2(1,0) = 0;
+    p2(2,0) = 0;
+    p2(3,0) = 1;
+
+    Eigen::MatrixXd result = T0 * p;
+    position_.setX(result(0,0));
+    position_.setY(result(1,0));
+    position_.setZ(result(2,0));
+
+    // result = j2 * p2;
+
+    // position2_.setX(result(0,0));
+    // position2_.setY(result(1,0));
+    // position2_.setZ(result(2,0));
+
+    // result = j3 * p2;
+
+    // position3_.setX(result(0,0));
+    // position3_.setY(result(1,0));
+    // position3_.setZ(result(2,0));
+}

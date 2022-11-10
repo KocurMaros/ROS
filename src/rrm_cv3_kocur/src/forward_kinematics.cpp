cd2459035c7e17201d41ee0371be551ee34f0ca3
KocurMaros <kocur.maros@gmail.com>
2022-11-10 13:07:15 +0100

    orient

--- src/rrm_cv3_kocur/src/forward_kinematics.cpp
+++ src/rrm_cv3_kocur/src/forward_kinematics.cpp
@@ -107,10 +107,10 @@ void ForwardKinematics::jointCallback(const sensor_msgs::JointState::ConstPtr& m
     tf::Matrix3x3 tf3d_dh;
 
 
-    tf3d.getRotation(orientation_);
     tf3d.setValue(static_cast<double>(T0(0,0)), static_cast<double>(T0(0,1)), static_cast<double>(T0(0,2)),
                   static_cast<double>(T0(1,0)), static_cast<double>(T0(1,1)), static_cast<double>(T0(1,2)),
                   static_cast<double>(T0(2,0)), static_cast<double>(T0(2,1)), static_cast<double>(T0(2,2)));
+    tf3d.getRotation(orientation_);
     tf3d_to_joint2.setValue(static_cast<double>(J2(0,0)), static_cast<double>(J2(0,1)), static_cast<double>(J2(0,2)),
                   static_cast<double>(J2(1,0)), static_cast<double>(J2(1,1)), static_cast<double>(J2(1,2)),
                   static_cast<double>(J2(2,0)), static_cast<double>(J2(2,1)), static_cast<double>(J2(2,2)));

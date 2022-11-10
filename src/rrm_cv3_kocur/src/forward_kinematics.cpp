a225a17ae9f9a1bbbb6d0aec28eb017de9b527ef
KocurMaros <kocur.maros@gmail.com>
2022-11-10 13:27:12 +0100

    DH

--- src/rrm_cv3_kocur/src/forward_kinematics.cpp
+++ src/rrm_cv3_kocur/src/forward_kinematics.cpp
@@ -33,15 +33,15 @@ void ForwardKinematics::broadcastTf(){
     transform.setRotation(q);
     broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "joint_1"));
     
-    transform.setOrigin( tf::Vector3(0, 0, 0.203));
-    q.setRPY(0,joint_state_.position[1],0);
-    transform.setRotation(q);
-    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_1", "joint_2"));
+    // transform.setOrigin( tf::Vector3(0, 0, 0.203));
+    // q.setRPY(0,joint_state_.position[1],0);
+    // transform.setRotation(q);
+    // broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_1", "joint_2"));
 
-    transform.setOrigin( tf::Vector3(0, 0, 0.178 +joint_state_.position[2]));
-    q.setRPY(0,0,0);
-    transform.setRotation(q);
-    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_2", "joint_3"));
+    // transform.setOrigin( tf::Vector3(0, 0, 0.178 +joint_state_.position[2]));
+    // q.setRPY(0,0,0);
+    // transform.setRotation(q);
+    // broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_2", "joint_3"));
 
     transform.setOrigin( tf::Vector3(0, 0, 0.178));
     q.setRPY(0,joint_state_.position[3],0);
@@ -49,21 +49,21 @@ void ForwardKinematics::broadcastTf(){
     broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"joint_3", "joint_4"));
 
     // Calculated forward kinematic tool0 -> base_link
-    transform.setOrigin( position_ );
-    transform.setRotation(orientation_);
-    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "tool0"));
-
-    // transform.setOrigin( position1_ );
-    // transform.setRotation(orientation1_);
-    // broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "joint_2"));
-
-    // transform.setOrigin( position2_ );
-    // transform.setRotation(orientation2_);
-    // broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "joint_3"));
-    
-    // transform.setOrigin( position3_ );
-    // transform.setRotation(orientation3_);
+    // transform.setOrigin( position_ );
+    // transform.setRotation(orientation_);
     // broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "tool0"));
+
+    transform.setOrigin( position1_ );
+    transform.setRotation(orientation1_);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "joint_2"));
+
+    transform.setOrigin( position2_ );
+    transform.setRotation(orientation2_);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "joint_3"));
+    
+    transform.setOrigin( position3_ );
+    transform.setRotation(orientation3_);
+    broadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"base_link", "tool0"));
     // Links
     transform.setOrigin( tf::Vector3(0, 0, 0.1015));
     q.setRPY(0,0,0);

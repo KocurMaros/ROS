159cbcf6c99d7b02237e9d8a59a27e81a0712cf2
laptop <kocur.maros@gmail.com>
2022-11-12 13:50:51 +0100

    roboty

---
+++ build/rrm_cv3_ridilla/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rrm/ROS/src/rrm_cv3_ridilla
+# Build directory: /home/rrm/ROS/build/rrm_cv3_ridilla
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/rrm_cv3_szepvolgyi/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rrm/ROS/src/rrm_cv3_szepvolgyi
+# Build directory: /home/rrm/ROS/build/rrm_cv3_szepvolgyi
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

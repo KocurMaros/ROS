8194954d4396b3d0e87306c3ade6823001f852ce
KocurMaros <kocur.maros@gmail.com>
2022-11-29 21:53:47 +0100

    orientation

--- src/abb_config/src/moveit_tutorial.cpp
+++ src/abb_config/src/moveit_tutorial.cpp
@@ -367,8 +367,10 @@ int main(int argc, char** argv)
 
   waypoints.push_back(start_pose2);
 
-
-  // target_pose3.orientation.w = 1.0;
+  target_pose3.orientation.x = 0.0;
+  target_pose3.orientation.y = 0.0;
+  target_pose3.orientation.z = 0.0;
+  target_pose3.orientation.w = 0.70711;
   target_pose3.position.x += 0.2;
   target_pose3.position.y -= 0.0;
   target_pose3.position.z -= 0.2;

7fbbf428525401de2b39b486ce5c5c52ddcc01f6
KocurMaros <kocur.maros@gmail.com>
2022-11-29 10:27:01 +0100

    resize ne toto

--- src/abb_config/src/moveit_tutorial.cpp
+++ src/abb_config/src/moveit_tutorial.cpp
@@ -106,7 +106,7 @@ int main(int argc, char** argv)
 
   moveit_msgs::CollisionObject collision_object;
   collision_object.meshes.resize(1);
-  collision_object.mesh_poses.resize(10);
+  collision_object.mesh_poses.resize(1);
   collision_object.meshes[0] = mesh;
   collision_object.header.frame_id = move_group_interface.getPlanningFrame();
   collision_object.mesh_poses[0].position.x = 1.0;

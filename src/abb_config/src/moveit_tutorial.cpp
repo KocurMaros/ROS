493f93175c76f83a6f07c0805de1dc543bbebc35
KocurMaros <kocur.maros@gmail.com>
2022-11-29 22:20:42 +0100

    z - 70722

--- src/abb_config/src/moveit_tutorial.cpp
+++ src/abb_config/src/moveit_tutorial.cpp
@@ -367,16 +367,25 @@ int main(int argc, char** argv)
 
   waypoints.push_back(start_pose2);
 
-  target_pose3.position.x += 0.2;
+  start_pose2.orientation.x = 0.0;
+  start_pose2.orientation.y = 0.70711;
+  start_pose2.orientation.z = -0.70711;  
+  start_pose2.orientation.w = 0.70711;
+  target_pose3.position.x += 0.0;
   target_pose3.position.y -= 0.0;
-  target_pose3.position.z -= 0.2;
+  target_pose3.position.z -= 0.0;
   waypoints.push_back(target_pose3);  // down
 
-  // target_pose3.orientation.w = 0.5;
-  target_pose3.position.x -= 0.2;
-  target_pose3.position.y += 0.0;
-  target_pose3.position.z -= 0.0;
-  waypoints.push_back(target_pose3);  // right
+  // target_pose3.position.x += 0.2;
+  // target_pose3.position.y -= 0.0;
+  // target_pose3.position.z -= 0.2;
+  // waypoints.push_back(target_pose3);  // down
+
+  // // target_pose3.orientation.w = 0.5;
+  // target_pose3.position.x -= 0.2;
+  // target_pose3.position.y += 0.0;
+  // target_pose3.position.z -= 0.0;
+  // waypoints.push_back(target_pose3);  // right
   
   // target_pose3.position.x = 0.55598;
   // target_pose3.position.y = 0.62698;

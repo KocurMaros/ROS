3948724a78c2ce6b04e4f2db8918d16f453d44f6
KocurMaros <kocur.maros@gmail.com>
2022-12-04 17:30:38 +0100

    u add

--- src/rrm_cv9/trajectory_visualization/src/trajectory_visualization.cpp
+++ src/rrm_cv9/trajectory_visualization/src/trajectory_visualization.cpp
@@ -67,6 +67,7 @@ int main(int argc, char **argv)
     // }
 
     // first state
+    double u=0;
     for (double t = 0; t <= 4; t += 0.1) {
 
         // Vytvorenie prejazdoveho bodu
@@ -94,7 +95,8 @@ int main(int argc, char **argv)
             point.accelerations[2] = 0;
         }
         else{
-            point.positions[2] = 30*M_PI/180 - t*30*M_PI/180;
+            u += 0.1;
+            point.positions[2] = 30*M_PI/180 - u*30*M_PI/180;
             point.velocities[2] = 0;
             point.accelerations[2] = 0;
         }
